f5d36d164c761cc09f3323be3bdaf6a85b498b6e
chandarvijay <chandar.vijay@icloud.com>
2025-04-04 10:23:28 +0530

    added toggle switch and added ultrsonic sensor code

--- esp32TestScript.ino
+++ esp32TestScript.ino
@@ -1,24 +1,35 @@
 
 #include "WiFi.h"
 #include "WebServer.h"
+#include "WebSocketsServer.h"
 
 
-int led = 5;
-int builtinled = T2;
-int Relay = 15;
+//int builtinled = T2;
+//int Relay = 15;
 const char* ssid = "Uma Nelayam 4G";
 const char* password = "9980337734";
 
+
+float distance;
+float duration;
+
+int triggerpin = 15;
+//int relay = 5;
+int echo =4 ;
+
+
+
+
+
+
 WebServer server(80);
 
-
+WebSocketsServer WS = WebSocketsServer(81);
 
 
 void welcome() {
 
   char test[] PROGMEM = R"===(<!DOCTYPE html>
-
-
 
 <html lang="en">
 <head>
@@ -27,7 +38,6 @@ void welcome() {
     <title>Test Page</title>
     <script src="script.js" defer></script>
 <style>
-
 h1{
 background-color: rgb(99, 99, 176);
 border: 2px solid black;
@@ -61,8 +71,79 @@ button{
 
 }
 
-
+.switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
+  
+  .switch input { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+  
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ccc;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
+  
+  input:checked + .slider {
+    background-color: #2196F3;
+  }
+  
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196F3;
+  }
+  
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
+  
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+  
+  .slider.round:before {
+    border-radius: 50%;
+  }
 </style>
+
+
+<!DOCTYPE html>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js" async></script>
 
 </head>
 <body>
@@ -74,8 +155,15 @@ button{
             <h2>SUMP Water Level</h2>
             <p>Sump tank that is pumped up to the water tank</p>
             <label for="fname">Water Percentage</label><br>
-            <input type="text" id="fname" name="fname"><br><br>
-            <progress id="file" value="90" max="100"> 90% </progress><br><br>
+            <input type="text" id="swaterper" name="fname" value="%"><br><br>
+
+            <progress id="file" value="%" max="100"> 90% </progress><br><br>
+
+            <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label> <br>
+
 
             <label for="fname">Motar Manual Operations</label><br>
             <button>ON</button>
@@ -86,12 +174,19 @@ button{
             <h2>Sintex Water Tank</h2>
             <p>Water Tank Place on the Builing </p>
             <label for="fname">Water Percentage</label><br>
-            <input type="text" id="fname" name="fname"><br><br>
-            <progress id="file" value="10" max="100"> 90% </progress><br><br>
+            <input type="text" id="twaterper" name="fname" value="%"><br><br>
+            <progress id="watertankper" value ="%" max="100"> 90% </progress><br><br>
+
+            <label class="switch">
+                <input type="checkbox" checked>
+                <span class="slider round"></span>
+              </label> <br>
+
 
             <label for="fname">Motar Manual Operations</label><br>
-            <button>ON</button>
-            <button>OFF</button>
+            
+            <button onclick="motaron">ON</button>
+            <button onclick="motaroff">OFF</button>
         </section>
     </main>
     <footer>
@@ -99,31 +194,10 @@ button{
     </footer>
 
 
+</html>
 
 
-
-
-</html>)===";
-
-  //String test = "<!DOCTYPE html>\n";
-  //test += " <header><h1 style=background-color: rgb(99, 99, 176);>WaterLevel Controller </h1></header>\n";
-  //test += "<title>my webpage</title>";
-  //test += "<section><h2>SUMP Water Level</h2>";
-  //test+=" <p>Sump tank that is pumped up to the water tank</p>";
-  //test +=  "<input type='text'> ";
-  //test += " <progress value='90'> 90% </progress><br>";
-  //test += " <label>Motar Manual Operations</label><br>";
-  //test+= "  <button>ON</button>";
-  //test += " <button>OFF</button></section>";
- //test += "<section><h2> Water Tank </h2>";
-  //test+=" <p>Sump tank that is pumped up to the water tank</p>";
-  ///test +=  "<input type='text'> ";
- // test += " <progress value='10'> 90% </progress><br>";
-  //test += " <label>Motar Manual Operations</label><br>";
-  //test+= "  <button>ON</button>";
- // test += " <button>OFF</button></section>";
-
-
+)===";
 
  server.send(200,"text/html",test);
 
@@ -134,7 +208,7 @@ void setup() {
 
 
   Serial.begin(115200);
-
+  
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   delay(100);
@@ -158,26 +232,31 @@ void setup() {
 
 
   // put your setup code here, to run once:
-
-  pinMode(led, OUTPUT);
-  pinMode(builtinled, OUTPUT);
-  pinMode(Relay, OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(triggerpin,OUTPUT);
+ // pinMode(led, OUTPUT);
+ // pinMode(Relay, OUTPUT);
 }
 void loop() {
 
+//Ultrasonic 
+
+digitalWrite(triggerpin,LOW);
+delayMicroseconds(20);
+
+duration = pulseIn(echo,HIGH);
+distance =(duration/2)*0.343;
+
+Serial.print("The distance is \n");
+Serial.print(distance);
+
+
+
+
+//Webserver 
   server.handleClient();
   server.send(200, "text/html");
   server.send(404, "text/html");
 
 
-
-  digitalWrite(builtinled, LOW);
-  delay(500);
-  digitalWrite(builtinled, LOW);
-  delay(200);
-  digitalWrite(led, HIGH);
-  delay(200);
-  digitalWrite(led, LOW);
-  delay(2000);
-  // put your main code here, to run repeatedly:
 }
